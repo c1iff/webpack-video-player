af8ba9a97bf9ff8f88925679b4f66b0c8a710faa
Chris Clifford <cclifford82@gmail.com>
2017-03-30 19:48:37 -0700

    adds player implementation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
+import $ from 'jquery';
 import Chart from 'chart.js';
 import css from './vendor.js';
 import style from '../styles/styles.scss'
+import videoStyle from '../styles/video.scss'
+
+$(document).ready(function() {
+  $("#changePlayerColor").click(function(){
+    var color = $('#picker').val()
+    color = "blue"
+    alert('change color', color)
+    $(".vjs-big-play-button:active").css("color", color);
+  })
+})
